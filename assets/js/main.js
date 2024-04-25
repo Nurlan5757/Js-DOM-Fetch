1daf40bc7f6c22e34e88a550dcd62698df1287cf
unknown <tu4pmkvys@code.edu.az>
2024-04-26 01:49:51 +0400

    tapsiriq

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+function search() {
+    var searchInput = document.getElementById('searchInput').value;
+    document.getElementById('searchResults').innerHTML = "Axtariş nəticələri: " + searchInput;
+  }
